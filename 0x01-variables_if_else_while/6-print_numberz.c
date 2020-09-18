854bf4b3ef65e8e7038fdd364c9d631ec7afdf56
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-09-18 00:24:43 +0000

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int numberz = 48;
+
+	while (numberz <= 57)
+	{
+		putchar(numberz);
+		numberz++;
+	}
+	putchar(10);
+	return (0);
+}
