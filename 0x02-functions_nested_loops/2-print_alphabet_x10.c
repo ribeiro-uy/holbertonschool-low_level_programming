546d67d79ab7656619d430c3fb887519a043ebea
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-09-22 18:28:02 +0000

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,7 +12,7 @@ void print_alphabet_x10(void)
 	char letra;
 	int diez;
 
-	for (diez = 0; diez <= 10; diez++)
+	for (diez = 0; diez <= 9; diez++)
 	{
 		for (letra = 97; letra <= 122; letra++)
 		{
