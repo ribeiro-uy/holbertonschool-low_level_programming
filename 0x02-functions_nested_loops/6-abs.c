ea56cf42fa40556d8409df4059fca2946f5e513f
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-09-22 16:13:05 +0000

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,18 +2,15 @@
 
 /**
  * _abs - return the absolute number
- * n - number value
+ * @n: Number to check
  * Return: The absolute value
  */
 
 int _abs(int n)
 {
-	if (n > 0)
+	if (n < 0)
 	{
-		_putchar(n);
+		n = -n;
 	}
-	else
-	{
-		_putchar(n * (-1));
-	}
+	return (n);
 }
