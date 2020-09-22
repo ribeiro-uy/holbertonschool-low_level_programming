da070a1798e2d6b54077f946eddeac153105cfe3
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-09-22 01:13:12 +0000

    ?

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int)
+
+{
+	_putchar (print_last_digit '%' '10')
+}
