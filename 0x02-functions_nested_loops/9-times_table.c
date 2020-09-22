93fc9fd9e05599ab14749fe495aa13130e3f8ae4
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-09-22 17:11:04 +0000

    9-times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ * times_table - prints 9 times table.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int prim, seg, resultado;
+
+	for (prim = 0; prim <= 9; prim++)
+	{
+		for (seg = 0; seg <= 9; seg++)
+		{
+			resultado = prim * seg;
+			if (resultado <= 9)
+			{
+				_putchar(resultado + 48);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((resultado / 10) + 48);
+				_putchar((resultado % 10) + 48);
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
