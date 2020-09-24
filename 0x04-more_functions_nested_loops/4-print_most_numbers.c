ab367802c340d1780320619c213fe4c01aab09bc
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-09-24 17:33:16 +0000

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,22 +4,19 @@
  * print_numbers - function that prints the numbers, from 0 to 9,
  * followed by a new line.
  *
- * @num: number variable
+ *
  * Return: Always 0.
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int num;
 
-	for (num = 0; num <= 9; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		if (num == 2 || num == 4)
-			continue;
-		else
-			_putchar(num + '0');
+		if (num != '2' && num != '4')
+			_putchar(num);
 	}
-
 	_putchar('\n');
 
 }
