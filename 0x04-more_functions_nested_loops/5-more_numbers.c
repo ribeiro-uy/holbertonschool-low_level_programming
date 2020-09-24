661081cf8c9f7b086a6dcd9f113a09ca2aad5bbd
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-09-24 19:11:04 +0000

    5-more_numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,18 +12,20 @@
 
 void more_numbers(void)
 {
-	int numero, veces, decena = numero / 10, unidad = numero % 10;
+	int numero, veces, unidad, decena;
 
 	for (veces = 0; veces <= 10; veces++)
 	{
-		for (numero = 0, numero <= 14, numero++)
+		for (numero = 0; numero <= 14; numero++)
 		{
 			if (numero >= 10)
 			{
+				decena = (numero / 10);
 				_putchar(decena + '0');
 			}
+			unidad = (numero % 10);
 			_putchar(unidad + '0');
 		}
-		_putchar('\n')
+		_putchar('\n');
 	}
 }
