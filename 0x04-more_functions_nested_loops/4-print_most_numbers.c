1a9bb0757dfd5f89b449554a7a238b73a4ab6963
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-09-24 17:34:39 +0000

    4-print_most_numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_numbers - function that prints the numbers, from 0 to 9,
+ * print_most_numbers - function that prints the numbers, from 0 to 9,
  * followed by a new line.
  *
  *
