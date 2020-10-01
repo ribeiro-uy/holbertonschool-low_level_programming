ec5a72d31b2c7191b16942b7c328c6d761716aa7
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-01 19:27:53 +0000

    5-string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * string_toupper - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *string_toupper(char *letter)
+{
+
+	int counter;
+
+	for (counter = 0; letter[counter] != '\0'; counter++)
+	{
+		if (letter[counter] >= 97 && letter[counter] <= 122)
+			letter[counter] = letter[counter] - 32;
+	}
+	return (letter);
+}
