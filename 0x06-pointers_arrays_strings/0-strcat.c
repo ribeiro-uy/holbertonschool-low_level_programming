f75be8654d57c27156b85963140ecb74d217a98e
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-01 16:13:17 +0000

    0-strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strcat - function that concatenates two string.
+ *
+ * @dest: destiny
+ * @src: source
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len;
+	int i;
+
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+	{
+	}
+
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[dest_len + i] = src[i];
+	}
+		dest[dest_len + i++] = '\0';
+
+	return (dest);
+}
