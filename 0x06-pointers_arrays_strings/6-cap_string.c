a4fb83281bd24a4b739faa2434f1453e3939b5ff
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-02 00:15:00 +0000

    fix 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * letter - function that capitalizes all words of a string.
+ * cap_string - function that capitalizes all words of a string.
  * @letter: character
  * Return: Always 0.
  */
@@ -12,10 +12,20 @@ char *cap_string(char *letter)
 	int counter;
 
 	for (counter = 0; letter[counter] != '\0'; counter++)
-		if (counter = " " || "\t" || "\n" || "," || ";" || "."
-|| "!" || "?" || "," || "(" || ")" || "{" || "}")
+	{
+		if (letter[counter] == 44 || letter[counter] == 59 ||
+		    letter[counter] == 46 || letter[counter] == 9 ||
+		    letter[counter] == 10 || letter[counter] == 32 ||
+		    letter[counter] == 33 || letter[counter] == 33 ||
+		    letter[counter] == 63 || letter[counter] == 34 ||
+		    letter[counter] == 40 || letter[counter] == 41 ||
+		    letter[counter] == 123 || letter[counter] == 125)
 		{
-			letter[counter + 1] = letter[counter + 1] - 32;
+			if (letter[counter +1 ] >= 97 && letter[counter + 1] <= 122)
+			{
+				letter[counter + 1] = letter[counter + 1] - 32;
+			}
 		}
+	}
 	return (letter);
 }
