a6ba907931566759ba95d7efed155283b08c32a2
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-06 02:16:55 +0000

    5-strstr

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,25 +13,22 @@
 char *_strstr(char *haystack, char *needle)
 
 {
-
 	int i, ii;
 
-		for (ii = 0; haystack[ii] != '\0'; ii++)
+	if (*needle == 0)
+		return (haystack);
+
+	for (ii = 0; haystack[ii] != '\0'; ii++)
+	{
+		for (i = 1; needle[i] != '\0'; i++)
 		{
-			if (needle[0] == haystack[ii])
+			if (needle[i] != haystack[i + ii])
 			{
-				for (i = 1; needle[i] != '\0'; i++)
-				{
-					if (needle[i] != haystack[i + ii])
-					{
-						break;
-					}
-				}
-			}
-			if (needle[i + 1] == '\0')
-			{
-				return (haystack + ii);
+				break;
 			}
 		}
+		if (needle[i] == '\0')
+			return (haystack + ii);
+	}
 	return (NULL);
 }
