369758040a53352abe650b2781b1fd7ccf3e1d7a
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-07 17:33:28 +0000

    3-factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -12,10 +12,10 @@ int factorial(int n)
 {
 
 	if (n < 0)
-		return(-1);
+		return (-1);
 
 	if (n == 1)
-		return(1);
+		return (1);
 
-	return(n *factorial(n - 1));
+	return (n * factorial(n - 1));
 }
