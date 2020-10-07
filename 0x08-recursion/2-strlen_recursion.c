123d68fec8e67fdfd388ff1f6dc9606b24af60c6
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-07 17:13:53 +0000

    2-strlen_recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ *
+ * @s: string
+ *
+ * Return: Always 0.
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s == '\0')
+		return(0);
+
+	len = (_strlen_recursion(s + 1) + 1);
+
+	return(len);
+}
