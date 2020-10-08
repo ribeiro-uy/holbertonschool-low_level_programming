6e942ddeb58202864ef18056b16ae606e79f3238
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-08 16:56:09 +0000

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,8 @@
 #include "holberton.h"
+#include <stdio.h>
 
 /**
- * _is_prime_number - function that returns 1 if the input
- * integer is a prime number, otherwise return 0.
- * to the power of y.
+ * is_prime_number - check the code for Holberton School students.
  *
  * @n: prime number
  *
@@ -12,16 +11,29 @@
 
 int is_prime_number(int n)
 {
+	if (n <= 1)
+		return (0);
 
-        if (n < 0)
-        {
-		return (-1);
-        }
+	return (yo(n, n - 1));
+}
 
-	if (y == 0)
-        {
-                return (1);
-	}
+/**
+ * yo - my own function
+ *
+ * @n: variable
+ * @b: variable
+ *
+ * Return: Always 0.
+ */
 
-	return (n * (_pow_recursion(x, y - 1)));
+
+int yo(int n, int b)
+{
+	if (b == 1)
+		return (1);
+
+	if (n % b == 0)
+		return (0);
+
+	return (yo(n, b - 1));
 }
