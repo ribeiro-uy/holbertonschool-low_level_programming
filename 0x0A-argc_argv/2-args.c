94ffafff09684115448bf3e7a1849a627ebb6742
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-10 22:46:40 +0000

    2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line.
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Nothing.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
