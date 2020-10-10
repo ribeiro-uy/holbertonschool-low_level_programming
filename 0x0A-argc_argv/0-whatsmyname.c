8c9f2ea474c8e525d05bf01bf253b015ef8cd55c
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-10 21:28:35 +0000

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name, followed by a new line.
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Nothing.
+ */
+
+int main(int argc, char argv)
+{
+	(void)argc;
+
+	printf("%s", argv[0]);
+}
