dce18795c80f2dea814816dda6e9f44c796b390f
Roberto Ribeiro <roberto@debian>
2020-10-19 14:39:28 -0300

    Task 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/**
+ * struct dog - struct dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Description: struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
