c104f881dda81fe247e72df9e2936a15857858c7
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-25 01:40:41 +0000

    variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,30 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+
+/**
+ * struct qlito - calling for each element and compare
+ * @bomba: type of data received from arguments
+ * @funk: pointer to functions to execute
+ * Description: This takes the char from arguments
+ */
+typedef struct qlito
+{
+	char *bomba;
+	void (*funk)();
+} rebelion;
+void pechar(char *cye, va_list magicians);
+void pinta(char *cye, va_list magicians);
+void fumeta(char *cye, va_list magicians);
+void sublingual(char *cye, va_list magicians);
+#endif
 
 #endif
