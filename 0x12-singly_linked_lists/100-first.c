6a2e7d778f7506d8ab8f2f23a6423808578eef88
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-29 20:00:29 -0300

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,12 +6,11 @@
  * you must allow,\nI bore my house upon my back!\n
  * before the main function is executed.
  *
+ * Description: print before the main
  * Return: Nothing
  */
 
-void __start(void) __attribute__((constructor));
-
-void __start(void)
+void __start(void) __attribute__((constructor))
 {
 printf("You're beat! and yet, you must allow,\n");
 printf("I bore my house upon my back!\n");
