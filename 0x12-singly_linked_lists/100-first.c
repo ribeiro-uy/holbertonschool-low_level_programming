7a8f4e972e1edf3c3310cc34df1f6d86f933f0ed
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-29 20:19:52 -0300

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 #include <stdio.h>
 
+void premain(void) __attribute__ ((constructor));
+
 /**
  * premain - function that prints You're beat! and yet,
  * you must allow,\nI bore my house upon my back!\n
