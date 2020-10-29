21d6c1e097362f3f087794246cc7faebb8c280f3
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-29 20:16:16 -0300

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,27 +2,15 @@
 #include <stdio.h>
 
 /**
- * premain - unction that prints You're beat! and yet,
+ * premain - function that prints You're beat! and yet,
  * you must allow,\nI bore my house upon my back!\n
  * before the main function is executed.
  *
  * Return: Nothing
  */
 
-void __attribute__ ((constructor)) premain()
+void premain(void)
 {
-	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
-}
-
-
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-
-int main(void)
-{
-	return (0);
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
