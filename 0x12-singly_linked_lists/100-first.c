48b7ea3f8b1fad03649878fa5b805a721265c305
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-29 19:54:03 -0300

    100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * __start - unction that prints You're beat! and yet,
+ * you must allow,\nI bore my house upon my back!\n
+ * before the main function is executed.
+ *
+ * Return: Nothing
+ */
+
+void __start(void) __attribute__((constructor));
+
+void __start(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
