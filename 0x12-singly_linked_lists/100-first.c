df4ec4c01d25e044fb5097174e02c5fd8e8b0ec9
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-29 20:17:24 -0300

    100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -11,6 +11,6 @@
 
 void premain(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
