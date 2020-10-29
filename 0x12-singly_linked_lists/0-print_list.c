b36dc5d70167e1837e48c689a33ce8a7d6ee15f9
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-10-29 07:01:39 -0300

    0x12 project

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ *
+ * @h: pointer to head of the list
+ *
+ * Return: Always 0.
+ */
+
+size_t print_list(const list_t *h)
+{
+	int i;
+
+	while (!h)
+	{
+		_putchar(
+		h = h->next;
+	}
