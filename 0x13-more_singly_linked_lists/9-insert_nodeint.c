308ab61c60ddc9efdfc6da30ddbefc7dbf2b3d5d
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-11-04 14:22:20 -0300

    fix 92

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		while (i != (idx - 1))
 		{
-			i++ ;
+			i++;
 			ptr = ptr->next;
 		}
 		new->next = ptr->next;
