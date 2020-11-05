bcd306f436bb9ccec74db3aa987be47b232ee55c
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-11-05 15:51:48 -0300

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * binary_to_unit - function that converts a binary number to an unsigned int.
+ * binary_to_uint - function that converts a binary number to an unsigned int.
  *
  * @b: binary to convert
  *
