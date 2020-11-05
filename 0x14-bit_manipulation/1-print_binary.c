b4a50c9dbe2f9df3aa87cb527c5c2c26009cdc20
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-11-05 17:33:22 -0300

    update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,21 +8,24 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int i, res, flag = 0;
+	long int i, res, flag = 0;
 
-	for (i = 32; i > 0; i--)
+	if (n == 0)
+		_putchar('0');
+
+	for (i = 32; i >= 0; i--)
 	{
 		res = n >> i;
 
 		if (res & 1)
 		{
 			flag = 1;
-			putchar('1');
+			_putchar('1');
 		}
 		else
 			if (flag == 1)
 			{
-				putchar('0');
+				_putchar('0');
 			}
 	}
 }
