4ac8888392d986e5d8dcf4a11e358719284ff892
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-11-05 17:29:19 -0300

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,12 +3,12 @@
  * print_binary - function that prints the binary representation of a number.
  *
  * @n: binary to convert
- * Return: Always 0.
+ *
  */
 
 void print_binary(unsigned long int n)
 {
-	int i, res, flag = 0;
+	unsigned long int i, res, flag = 0;
 
 	for (i = 32; i >= 0; i--)
 	{
@@ -26,14 +26,3 @@ void print_binary(unsigned long int n)
 			}
 	}
 }
-
-/**
- * {
- *
- *	if (n == 0)
- *		return;
- *
- *	print_binary(n/2);
- *	putchar((n % 2) + '0');
- *}
- */
