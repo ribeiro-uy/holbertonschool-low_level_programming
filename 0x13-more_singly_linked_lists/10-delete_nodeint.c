eec8d6a5feea864c3ebbd837a2ca36376c0842a7
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-11-05 06:25:43 -0300

    fix 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *actual = *head;
 	listint_t *anterior = *head;
 
-	if (!head)
+	if (!*head)
 		return (-1);
 
 	if (index == 0)
