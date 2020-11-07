4592e92c88bd9dfc619f0ba622f26a79d439c13d
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-11-07 16:11:55 -0300

    100-reverse_listint.c

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -1,35 +1,31 @@
 #include "lists.h"
 
 /**
- * delete_nodeint_at_index - function that deletes the node
- * at index index of a listint_t linked list.
+ * reverse_listint - function that reverses a listint_t linked list.
  *
  * @head: pointer to a pointer to the head of the list
- * @index:  is the index of the node that should be deleted. Index starts at 0
  *
- * Return: Always 0.
+ * Return: reverse list
  */
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *actual = *head;
-	listint_t *futuro = (*head)->next;
+	listint_t *current = *head;
+	listint_t *future = (*head)->next;
+	(*head)->next = NULL;
 
 	if (!*head && !(*head)->next)
 		return (NULL);
 
-	while ((*head) != NULL)
+	while (future->next != NULL)
 	{
-		printf("entre en el while\n");
-		if (!futuro->next)
-		{
-			printf("el futuro es ahora\n");
-			return (*head);
-		}
-		*head = futuro;
-		futuro = futuro->next;
-		(*head)->next = actual;
-		actual = *head;
+		*head = future;
+		future = future->next;
+		(*head)->next = current;
+		current = *head;
 	}
+
+	*head = future;
+	(*head)->next = current;
 	return (*head);
 }
