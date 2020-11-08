640e5db300d378871bfa50695098da85edc620d7
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-11-08 01:14:32 -0300

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
-B#include <stdio.h>
-/*
- * Error
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Entry point
  *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-fprintf(stderr, "and that piece of art is useful" - Dora Korpar, 2015-10-19(\n");
-returne (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
