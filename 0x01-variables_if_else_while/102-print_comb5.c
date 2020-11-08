a12a81538480402d7f1af1a670be1313c36594b9
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-11-08 00:52:50 -0300

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,42 +8,43 @@
 
 int main(void)
 {
-	int unit = 0, decimal = 0;
-	int u = 0, d = 0;
-	while (d <= 9)
-	{
-		while (u <= 9)
-		{
-			while (decimal <= 9)
-			{
-				while (unit <= 9)
-				{
-					if (d < decimal || (d == decimal && u < unit)) 
-					{
-						putchar(d + '0');
-						putchar(u + '0');
-						putchar(' ');
-						putchar(decimal + '0');
-						putchar(unit + '0');
-						if (d == '9' && u == 8 && decimal == '9' && unit == '9')
-						{}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-					unit++;
-				}
-				unit = 0;
-				decimal++;
-			}
-			decimal = 0;
-			u++;
-		}
-		u = 0;
-		d++;
-	}
-	putchar('\n');
-	return (0);
+int unit = 0, decimal = 0;
+int u = 0, d = 0;
+
+while (d <= 9)
+{
+while (u <= 9)
+{
+while (decimal <= 9)
+{
+while (unit <= 9)
+{
+if (d < decimal || (d == decimal && u < unit))
+{
+putchar(d + '0');
+putchar(u + '0');
+putchar(' ');
+putchar(decimal + '0');
+putchar(unit + '0');
+if (d == '9' && u == 8 && decimal == '9' && unit == '9')
+{}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+unit++;
+}
+unit = 0;
+decimal++;
+}
+decimal = 0;
+u++;
+}
+u = 0;
+d++;
+}
+putchar('\n');
+return (0);
 }
