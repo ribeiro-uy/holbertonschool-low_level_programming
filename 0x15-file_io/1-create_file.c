f35a96d3c11e7dbc8413607b2bda10751eb3caa7
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-11-09 18:00:01 -0300

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0, fd;
+	int i = 0, fd, wwrite, cclose;
 
 	if (filename == NULL)
 		return (-1);
@@ -21,13 +21,22 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	/* text is empty if the content is null */
+	if (text_content == NULL)
+		text_content = "";
+
 	while (text_content[i] != '\0')
 	{
 		i++;
 	}
 
-	write(fd, text_content, i);
+	wwrite = write(fd, text_content, i);
+	if (wwrite ==  -1)
+		return (-1);
 
-	close(fd);
+	cclose = close(fd);
+	if (cclose == -1)
+		return (-1);
+	
 	return (1);
 }
