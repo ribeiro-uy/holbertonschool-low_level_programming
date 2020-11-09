5500dffaea6f540096d6451d9f74db05aef625d8
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-11-09 18:47:39 -0300

    2-append_text_to_file.c

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int i = 0, fd;
 
-	if (filename == NULL && text_content == NULL)
+	if (filename == NULL || text_content == NULL)
 		return (1);
 
 	/* create */
