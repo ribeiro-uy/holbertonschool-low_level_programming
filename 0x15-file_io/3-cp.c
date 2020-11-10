029ef9e87470d5d0e1cc55e3b84353962709649c
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-11-09 23:00:23 -0300

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -15,18 +15,12 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	fd1 = open(argv[1], O_RDONLY);
 	leer = read(fd1, buff, 1024);
-	if (leer == -1 || fd1 == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd1);
-		exit(98);
-	}
+	if (fd1 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	escribir = write(fd2, buff, leer);
-	if (fd2 == -1 || escribir == -1)
-	{dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", argv[2]);
-		close(fd2);
-		exit(99);
-	}
+	if (fd2 == -1)
+		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", argv[2]), exit(99);
 	while (leer == 1024)
 	{
 		leer = read(fd1, buff, 1024);
