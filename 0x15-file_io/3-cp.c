6879c1a496d9bfc67ea5401d4b03ffa30e2bc576
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-11-10 19:04:46 +0000

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
 	fd2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	escribir = write(fd2, buff, leer);
 	if (fd2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", argv[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]), exit(99);
 	while (leer == 1024)
 	{
 		leer = read(fd1, buff, 1024);
@@ -41,11 +41,9 @@ int main(int argc, char **argv)
 		exit(99);
 		}
 	}
-	close1 = close(fd1);
-	close2 = close(fd2);
-	if (close1 == -1)
+	if (close(fd1) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", close1), exit(100);
-	if (close2 == -1)
+	if (close(fd2) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i\n", close2), exit(100);
 	return (0);
 }
