79c4c703d02ad9a06ff9be18fb37044a75003625
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-13 15:27:48 +0000

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,25 +1,24 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * free_dlistint - function that frees a dlistint_t list.
  *
- * Return: Always EXIT_SUCCESS.
+ * @head: pointer to doble linked list.
+ *
+ * Return: Nothing.
  */
 void free_dlistint(dlistint_t *head)
 {
-        dlistint_t *tmp = NULL;
+	dlistint_t *tmp = NULL;
 
-        if (head == NULL)
-                return;
-
-        while (head->prev)
-                head = head->prev;
-
-        while (head)
-        {
-                tmp = head->next;
-                free(head);
-                head = tmp;
-        }
+	if (head == NULL)
+		return;
+	while (head->prev)
+		head = head->prev;
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
 }
-
