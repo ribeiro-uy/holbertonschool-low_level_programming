ca4896bc7d5dc285a81deb843259607cf2e45a89
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-13 15:37:53 +0000

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -13,6 +13,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
+	if (head == NULL)
+		return (NULL);
 	while (head->prev)
 		head = head->prev;
 	while (i <= index)
