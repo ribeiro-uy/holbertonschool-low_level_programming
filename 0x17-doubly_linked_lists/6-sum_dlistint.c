002152e9416f2106570edfbb0e61706a80e710c4
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-13 15:41:26 +0000

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,23 +1,25 @@
 #include "lists.h"
+
 /**
- * main - check the code for Holberton School students.
+ * sum_dlistint - function that returns the sum of all the data (n)
+ * of a dlistint_t linked list.
  *
- * Return: Always EXIT_SUCCESS.
+ * @head: pointer to a doble linked list.
+ *
+ * Return: sum and if the list is empty, return 0
  */
 int sum_dlistint(dlistint_t *head)
 {
-        int sum = 0;
+	int sum = 0;
 
-        if (head == NULL)
-                return (sum);
-
-        while (head->prev)
-                head = head->prev;
-
-        while (head)
-        {
-                sum += head->n;
-                head = head->next;
-        }
-        return (sum);
+	if (head == NULL)
+		return (sum);
+	while (head->prev)
+		head = head->prev;
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
 }
