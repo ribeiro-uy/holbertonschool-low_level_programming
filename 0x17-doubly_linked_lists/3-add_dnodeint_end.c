ebee5ba13868c839d606df92a135644361923bff
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-13 15:24:27 +0000

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,37 +1,35 @@
 #include "lists.h"
 
 /**
- * main - function that adds a new node at the end of a dlistint_t list.
- * @head = pointer to a head
- * @n = new int in the node
- * Return: Always EXIT_SUCCESS.
+ * add_dnodeint_end - function that adds a new node
+ * at the end of a dlistint_t list.
+ *
+ * @head: pointer to doble linked list.
+ * @n: new int in the node.
+ *
+ * Return: the address of the new element, or NULL if it failed.
  */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-        dlistint_t *tmphead = *head, *newnode = NULL;
+	dlistint_t *tmphead = *head, *newnode = NULL;
 
-        if ((newnode = malloc(sizeof(dlistint_t))) == NULL)
-                return (NULL);
-
-        newnode->n = n;
-        newnode->next = NULL;
-
-        if (head == NULL)
-                return (NULL);
-
-        if (*head == NULL)
-        {
-                *head = newnode;
-                return(newnode);
-        }
-
-        while (tmphead->next)
-        {
-                tmphead = tmphead->next;
-        }
-
-        tmphead->next = newnode;
-        newnode->prev = tmphead;
-
-        return (newnode);
+	newnode = malloc(sizeof(dlistint_t));
+	if (newnode == NULL)
+		return (NULL);
+	newnode->n = n;
+	newnode->next = NULL;
+	if (head == NULL)
+		return (NULL);
+	if (*head == NULL)
+	{
+		*head = newnode;
+		return (newnode);
+	}
+	while (tmphead->next)
+	{
+		tmphead = tmphead->next;
+	}
+	tmphead->next = newnode;
+	newnode->prev = tmphead;
+	return (newnode);
 }
