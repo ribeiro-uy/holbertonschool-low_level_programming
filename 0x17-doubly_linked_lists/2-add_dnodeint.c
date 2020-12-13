93f577374ae6a2a93412417af4a4d87ad6514a6f
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-13 14:57:01 +0000

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		while ((*head)->prev != NULL)
 			*head = (*head)->prev;
 		newnode->next = *head;
-		(*head)->prev = node;
+		(*head)->prev = newnode;
 		*head = newnode;
 	}
 	return (*head);
