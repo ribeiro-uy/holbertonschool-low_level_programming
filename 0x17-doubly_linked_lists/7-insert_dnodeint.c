ae01ac757631f5aecafe4ebfb3de033eea911736
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2020-12-14 02:47:54 +0000

    0x17-doubly_linked_lists

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,47 +1,45 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
+ * insert_dnodeint_at_index - function that inserts a new node
+ * at a given position.
  *
- * Return: Always EXIT_SUCCESS.
+ * @h: doble pointer to a doble likned list.
+ * @idx: position to insert the new node.
+ * @n: value of the new node.
+ *
+ * Return: check the code for Holberton School students.
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-        unsigned int i = 0;
-        dlistint_t *newnode = NULL;
+	unsigned int i = 0;
+	dlistint_t *newnode = NULL;
 
-        if (h == NULL)
-                return (*h);
-
-        if (*h == NULL)
-        {
-                newnode = add_dnodeint(*&h, n);
-                return (newnode);
-        }
-
-        while ((*h)->prev)
-                *h = (*h)->prev;
-
-        while (i <= idx)
-        {
-                if (i == idx)
-                {
-                        newnode = malloc(sizeof(dlistint_t));
-                        newnode->n = n;
-                        newnode->prev = (*h)->prev;
-                        newnode->next = (*h);
-                        (*h)->prev = newnode;
-                        *h = newnode;
-                        prinf("el anterior vale %i", (*h)->next->n);
-                        while ((*h)->prev)
-                                *h = (*h)->prev;
-                        return (newnode);
-                }
-                *h = (*h)->next;
-                i += 1;
-        }
-        newnode = add_dnodeint_end(*&h, n);
-        while ((*h)->prev)
-                *h = (*h)->prev;
-        return (*h);
+	if (h == NULL)
+		return (*h);
+	if (*h == NULL)
+	{
+		newnode = add_dnodeint(*&h, n);
+		return (newnode);
+	}
+	while ((*h)->prev)
+		*h = (*h)->prev;
+	while (i <= idx)
+	{
+		if (i == idx)
+		{
+			newnode = malloc(sizeof(dlistint_t));
+			newnode->n = n;
+			newnode->prev = (*h)->prev;
+			newnode->next = *h;
+			(*h)->prev = newnode;
+			while ((*h)->prev)
+				*h = (*h)->prev;
+			return (*h);
+		}
+		*h = (*h)->next;
+		i += 1;
+	}
+	newnode = add_dnodeint_end(*&h, n);
+	return (*h);
 }
