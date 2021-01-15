b5b4b73120991c8e26c69cd9cb8e5e60ab995573
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-01-15 14:12:10 +0000

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -20,8 +20,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = (char *)key;
-	new_node->value = (char *)value;
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 	if (ht->array[index] == NULL)
 	{
 		new_node->next = NULL;
