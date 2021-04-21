a42503dc8b1bbd8c94883f688f062c6c498c8b69
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-04-21 17:24:04 -0300

    add new prototype

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -6,5 +6,5 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int find_index(int *array, int value, int low, int high, size_t size);
-
+void print_check(size_t index, int integ);
 #endif
