7f4cdf138a20f6d480eb52c42ed187d7e07f8cbe
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-04-21 19:18:25 -0300

    change if condition

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -15,38 +15,39 @@ int interpolation_search(int *array, size_t size, int value)
 	size_t hi = size - 1;
 	size_t pos = low + (((double)(hi - low) / (array[hi] - array[low])) * (value - array[low]));
 
-	if (array == NULL || size < 0)
-		return (-1);
-	if (pos >= size)
+	if (array != NULL || size > 0)
 	{
-		printf("Value checked array[%lu] is out of range\n", pos);
-		return (-1);
-	}
-	if (array[low] > value)
-	{
-		print_check(low, array[low]);
-		return (-1);
-	}
-	if (array[low] == value)
-	{
-		print_check(low, array[low]);
-		return (low);
-	}
-	while (low <= hi)
-	{
-		print_check(pos, array[pos]);
-		if (array[pos] == value)
-			return (pos);
-		if (array[pos] > value)
-			hi = pos - 1;
-		else
-			low = pos + 1;
-		pos = low + (((double)(hi - low) / (array[hi] - array[low])) * (value - array[low]));
+		if (pos >= size)
+		{
+			printf("Value checked array[%lu] is out of range\n", pos);
+			return (-1);
+		}
+		if (array[low] > value)
+		{
+			print_check(low, array[low]);
+			return (-1);
+		}
+		if (array[low] == value)
+		{
+			print_check(low, array[low]);
+			return (low);
+		}
+		while (low <= hi)
+		{
+			print_check(pos, array[pos]);
+			if (array[pos] == value)
+				return (pos);
+			if (array[pos] > value)
+				hi = pos - 1;
+			else
+				low = pos + 1;
+			pos = low + (((double)(hi - low) / (array[hi] - array[low])) * (value - array[low]));
 
+		}
 	}
+
 	return (-1);
 }
-
 
 /**
  * print_check - prints the value checked
